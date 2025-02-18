8f917cbf884be4f89e564b142a70484462a514af
slymnyldrm <slymn.yildirim98@gmail.com>
2025-02-18 15:18:36 +0400

    gitignore modification and commit id fix for submodules

--- build-LeafMC-Desktop-Release/CMakeFiles/d/ab836b62321c8c238d58e42b25f002b4074cc55e31b9de267fa920b330c4b550.d
+++
@@ -1,4 +0,0 @@
-libs/shapelib/shputils_autogen/timestamp: \
-  /home/droneleaf/software-stack/LeafMC/libs/shapelib/CMakeLists.txt \
-  /home/droneleaf/software-stack/LeafMC/libs/shapelib/shputils.c \
-  /opt/cmake-3.24.1/bin/cmake
